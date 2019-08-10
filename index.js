3e7482df2261d13187602c768d24afc988ffbf1b
Anna Dybas <a.m.dybas@gmail.com>
2019-08-10 23:01:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,3 +13,17 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase())
 };
+
+function sayHiToGrandma(string) {
+  if (string === "I love you, Grandma.")
+    {
+    return "I love you, too."
+    }
+  else (string.toUpperCase())
+  {
+      return "YES INDEED"
+    }
+    else if (string.toLowerCase()) {
+      return "I can't hear you!"
+    }
+}
